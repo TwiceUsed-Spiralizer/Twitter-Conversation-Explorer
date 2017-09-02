512780a647f34c2127f80f336994575f1eb5e7d5
P-WM <P-WM@users.noreply.github.com>
2017-09-03 00:15:05 +0100

    Add MongoDB example script

--- mongo.js
+++ mongo.js
@@ -1,12 +1,12 @@
 /*
- * elastic.js 
+ * mongo.js 
  * 
  * Example file, demonstrating usage of twitter stream
- * and elastic search.
+ * and MongoDB.
  */
 require('dotenv').config();
 
-const elasticsearch = require('elasticsearch');
+const MongoClient = require('mongodb').MongoClient;
 const client = require('twitter')({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
@@ -14,13 +14,12 @@ const client = require('twitter')({
   access_token_secret: process.env.TWITTER_TOKEN_SECRET,
 });
 
-const esClient = new elasticsearch.Client({
-  host: process.env.ELASTICSEARCH_HOST,
-});
-
 const stream = client.stream('statuses/filter', { language: 'en', filter_level: 'low', track: 'a,b,c, ' });
-stream.on('data', tweet => tweet && esClient.index({
-  index: 'index',
-  type: 'type',
-  body: tweet,
-}));
+
+let mongodb;
+MongoClient.connect(process.env.MONGODB_URI, (err, db) => {
+  if (err) throw (err);
+  mongodb = db.collection('tweets');
+  module.exports = mongodb;
+  stream.on('data', tweet => tweet && mongodb.insert(tweet));
+});
