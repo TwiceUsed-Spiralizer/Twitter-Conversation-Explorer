5a10906b86c22166d5b77352db63f5fe59c88399
P-WM <P-WM@users.noreply.github.com>
2017-09-05 00:45:53 +0100

    Add lookUsers helper function to twitter API

--- server/tweet-fetcher/populate-recipients.js
+++ server/tweet-fetcher/populate-recipients.js
@@ -1,11 +1,18 @@
 const mongoConnect = require('./mongo');
-const {forEach, map } = require('lodash');
+const { forEach, map } = require('lodash');
 
 mongoConnect()
   .then(tweetsDB =>
     tweetsDB.find({}).sort({ created_at: 1 }).limit(100).toArray()
   )
   .then((tweets) => {
-    const user_ids = new Set();
-    tweet
+    let userIds = new Set();
+    const pushUserIds = tweet => forEach(tweet.recipients, (recipient) => {
+      if (typeof recipient === 'string') {
+        userIds.add(recipient);
+      }
+    });
+    forEach(tweets, pushUserIds);
+    userIds = Array.from(userIds);
+    
   });
--- server/tweet-fetcher/twitter-client.js
+++ server/tweet-fetcher/twitter-client.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const twitterClient = require('twitter')({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
@@ -7,4 +8,16 @@ const twitterClient = require('twitter')({
 
 exports.stream = function twitterStream(statusFilter) {
   return twitterClient.stream('statuses/filter', statusFilter);
+};
+
+exports.lookupUsers = function lookupUsers(userIds) {
+  return new Promise((resolve, reject) => {
+    twitterClient.post('users/lookup', { user_id: userIds.join(',') }, (err, users) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(users);
+      }
+    });
+  });
 };
