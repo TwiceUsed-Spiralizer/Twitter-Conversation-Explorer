53cd7bd82a1a7148cb5f028678e327250bd4da65
P-WM <P-WM@users.noreply.github.com>
2017-09-05 22:54:58 +0100

    Add mongo tests

--- test/tweet-fetcher/unit-tests.js
+++ test/tweet-fetcher/unit-tests.js
@@ -1,8 +1,11 @@
+// Node modules
+const { fork } = require('child_process');
 // Testing libraries
 const { expect } = require('chai');
 // Modules to test
 const { User, Tweet } = require('../../server/tweet-fetcher/models');
 const TweetManager = require('../../server/tweet-fetcher');
+const mongoConnectTest = require('../../server/tweet-fetcher/mongo');
 // Mock data
 const tweets = require('./example-tweets');
 
@@ -20,4 +23,23 @@ describe('TweetManager', () => {
   expect(() => new TweetManager()).to.not.throw();
 });
 
+describe('Populate Recipients', () => {
 
+  it('Tests mongo insertion and removal', (done) => {
+    mongoConnectTest()
+      .then(tweetsDB =>
+        tweetsDB.insert(new Tweet(tweets[2]))
+          .then(() => tweetsDB.find({ id_str: '1' }).toArray())
+          .then(results => ({ results, tweetsDB }))
+      )
+      .then(({ results, tweetsDB }) => {
+        expect(results.length).to.equal(1);
+        expect(results[0].id_str).to.equal('1');
+        return tweetsDB.remove({ id_str: '1' });
+      })
+      .then((result) => {
+        expect(result.result.ok).to.equal(1);
+        done();
+      });
+  });
+});
