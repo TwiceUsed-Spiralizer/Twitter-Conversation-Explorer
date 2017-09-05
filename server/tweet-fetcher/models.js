223f285c6d92217c2c2cb7d5bdb1c1b36d49249a
P-WM <P-WM@users.noreply.github.com>
2017-09-05 02:37:23 +0100

    Add empty for no reply

--- server/tweet-fetcher/models.js
+++ server/tweet-fetcher/models.js
@@ -8,7 +8,7 @@ exports.Tweet = class Tweet {
     this.short_text = tweet.truncated ? tweet.text : null;
     this.sender = new exports.User(tweet.user);
     this.hashtags = tweet.entities.hashtags;
-    this.recipients = uniq(tweet.entities.user_mentions.map(user => user.id_str).concat(tweet.in_reply_to_user_id_str));
+    this.recipients = uniq(tweet.entities.user_mentions.map(user => user.id_str).concat(tweet.in_reply_to_user_id_str || []));
     this.recipients_processed = false;
     this.gender = false;
   }
