12829fedcc9e7cee7b75e9f61322b0c6fad5cf56
Scott Mitchell <smitche60@gmail.com>
2017-09-06 16:16:06 -0500

    Implement basic server

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 8080
+
+app.listen(port, () => {
+  console.log(`Listening on port: ${port}`)
+})
+
