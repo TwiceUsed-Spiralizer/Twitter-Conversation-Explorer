28cb1cb8481b4a76103de456a67757b73a40db2a
P-WM <P-WM@users.noreply.github.com>
2017-09-06 17:57:22 +0100

    Rename tweet-fetcher module

--- server/tweet-manager/index.js
+++ server/tweet-manager/index.js
@@ -1,51 +1,17 @@
 /*
- *  tweet-fetcher/index.js
+ *  tweet-manager/index.js
  *  Coordinates fetching and parsing pipeline.
  *  Exports TweetManager class, which instantiates all necessary objects and processes
  *  to fetch and parse tweets.
  */
 
-// Inject envinronment variables
-require('dotenv').config();
 // TCE Modules
 const mongoConnect = require('./mongo');
-const { Tweet } = require('./models.js');
-const twitterStream = require('./twitter-client').stream;
+const TweetFetcher = require('./tweet-fetcher');
 // Node modules
 const { fork } = require('child_process');
-const EventEmitter = require('events');
 const path = require('path');
 const throttle = require('lodash/throttle');
-
-const isCandidate = function isCandidate(tweet) {
-  return !/^RT @/.test(tweet.text)
-    && !tweet.retweeted_status
-    && ((tweet.entities && tweet.entities.user_mentions.length > 0) || tweet.in_reply_to_status_id);
-};
-
-class TweetFetcher extends EventEmitter {
-  constructor(tweetsDB, params = {}) {
-    super();
-    this.statusFilter = params.statusFilter || { language: 'en', track: 'a,e,i,o,u,y,A,E,I,O,U,Y, ' };
-    this.batchSize = params.batchSize || 500;
-    this.numTweets = -1;
-    this.tweets = new Array(this.batchSize);
-    this.tweetsDB = tweetsDB;
-    twitterStream(this.statusFilter).on('data', tweet => tweet && this.tweetHandler(tweet));
-  }
-
-  tweetHandler(tweet) {
-    if (isCandidate(tweet)) {
-      this.tweets[this.numTweets += 1] = new Tweet(tweet);
-    }
-    if (this.numTweets >= this.batchSize) {
-      this.tweetsDB.insert(this.tweets);
-      this.tweets = new Array(this.batchSize);
-      this.emit('tweets', this.numTweets);
-      this.numTweets = -1;
-    }
-  }
-}
 
 module.exports = class TweetManager {
   constructor() {
