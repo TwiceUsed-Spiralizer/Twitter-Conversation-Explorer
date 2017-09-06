3615dba6a6602b43531b94f71ab73584579b4071
P-WM <P-WM@users.noreply.github.com>
2017-09-06 06:09:53 +0100

    Basic script to find gender

--- server/tweet-fetcher/add-gender/gender-compute.py
+++ server/tweet-fetcher/add-gender/gender-compute.py
@@ -1,7 +1,13 @@
-import sys, json
+import sys, os
+from unidecode import unidecode
+from pymongo import MongoClient
+from genderComputer.genderComputer import GenderComputer
 
-def do_the_thing():
-  print(sys.argv[1][::-1])
+def gender_names():
+  db = MongoClient(sys.argv[1]).tweets.tweets
+  gc = GenderComputer(os.path.abspath('./genderComputer/nameLists'))
+  for tweet in db.find({})[:100]:
+    print(gc.resolveGender(tweet[u'sender'][u'name'], ''))
 
 if __name__ == '__main__':
-  do_the_thing()
+  gender_names()
