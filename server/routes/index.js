fe6205d0587c0593a3863b495973ad37fb93ddb3
Scott Mitchell <smitche60@gmail.com>
2017-09-12 17:17:49 -0500

    Factor ES query body out of ES search function call

--- server/routes/index.js
+++ server/routes/index.js
@@ -12,26 +12,28 @@ const type = 'tweet';
 
 app.post('/api/KeywordAcrossGender', (req, res) => {
 
-const keyword = req.body.keyword || '*';
-
-client.search({
-    index,
-    type,
-    "size": 0,
-    "from": 0,
-    body: {
-      "aggs" : {
-        "interactions" : {
-          "adjacency_matrix" : {
-            "filters" : {
-              "femaleSender" : { "terms" : { 'sender.gender' : ["female"] }},
-              "maleSender" : { "terms" : { 'sender.gender' : ["male"] }},
-              "keyword" : { "wildcard" : { "full_text" : keyword } }
-            }
+  const keyword = req.body.keyword || '*';
+  const esBody =
+  {
+    "aggs" : {
+      "interactions" : {
+        "adjacency_matrix" : {
+          "filters" : {
+            "femaleSender" : { "terms" : { 'sender.gender' : ["female"] }},
+            "maleSender" : { "terms" : { 'sender.gender' : ["male"] }},
+            "keyword" : { "wildcard" : { "full_text" : keyword } }
           }
         }
       }
     }
+  }
+
+  client.search({
+    index,
+    type,
+    "size": 0,
+    "from": 0,
+    body: esBody
   }).then((body) => {
     return body.aggregations.interactions.buckets
   }).then((data) => res.send(data))
@@ -40,40 +42,40 @@ client.search({
 
 app.post('/api/SelectionsOverTime', (req, res) => {
 
-const keyword = req.body.keyword || '*';
-const senderGender = req.body.senderGender || false;
-const recipientsGender = req.body.recipientsGender || false;
-const esBody =
-{ "query": {
-  "bool": {
-    "must": [
-      { "wildcard" : { "full_text" : keyword } }
-    ]
-  }
-},
-  "aggs" : {
-    "histogram" : {
-      "date_histogram" : {
-          "field" : "created_at",
-          "interval" : "day"
+  const keyword = req.body.keyword || '*';
+  const senderGender = req.body.senderGender || false;
+  const recipientsGender = req.body.recipientsGender || false;
+  const esBody =
+  { "query": {
+    "bool": {
+      "must": [
+        { "wildcard" : { "full_text" : keyword } }
+      ]
+    }
+  },
+    "aggs" : {
+      "histogram" : {
+        "date_histogram" : {
+            "field" : "created_at",
+            "interval" : "day"
+        }
       }
     }
   }
-}
 
 // if senderGender exists we add senderGender to the musts
-if (senderGender) {
-  const toAdd = { match: { 'sender.gender': senderGender } }
-  esBody.query.bool.must.push(toAdd)
-}
+  if (senderGender) {
+    const toAdd = { match: { 'sender.gender': senderGender } }
+    esBody.query.bool.must.push(toAdd)
+  }
 
 // if recipientsGender exists we add recipientsGender to the musts
-if (recipientsGender) {
-  const toAdd = { match: { 'recipients.gender': recipientsGender } }
-  esBody.query.bool.must.push(toAdd)
-}
+  if (recipientsGender) {
+    const toAdd = { match: { 'recipients.gender': recipientsGender } }
+    esBody.query.bool.must.push(toAdd)
+  }
 
-client.search({
+  client.search({
     index,
     type,
     size: 0,
