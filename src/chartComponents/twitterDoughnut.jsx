f8d50706c270f47b514c16ba7c5d67f19bea2f87
gbatra17 <gbatra17@gmail.com>
2017-09-13 14:55:41 -0400

    rendering small doughnut

--- src/chartComponents/twitterDoughnut.jsx
+++ src/chartComponents/twitterDoughnut.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { Doughnut } from 'react-chartjs-2';
 
 const TwitterDoughnut = (props) => {
-  const data = props.data;
   console.log(props.data);
-  const womenPercent = (data.femaleSorry / data.female) * 100;
-  const menPercent = (data.maleSorry / data.male) * 100;
+
+  const womenPercent = (props.data[1].doc_count / props.data[0].doc_count) * 100;
+  const menPercent = (props.data[3].doc_count / props.data[4].doc_count) * 100;
   const total = womenPercent + menPercent;
   const womenLength = (womenPercent / total) * 600;
   const menLength = (menPercent / total) * 600;
