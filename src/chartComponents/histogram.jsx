ff64ae1df76576e27d327736ec44eea4d2cc5183
gbatra17 <gbatra17@gmail.com>
2017-09-13 19:43:03 -0400

    Rendering Histogram with dummy data

--- src/chartComponents/histogram.jsx
+++ src/chartComponents/histogram.jsx
@@ -1,8 +1,37 @@
 import React from 'react';
+import { Bar } from 'react-chartjs-2';
 
-export default props =>
-  (
-    <div className="hoverable">
-      {JSON.stringify(props.data)}
-    </div>
-  );
+const TCEHistogram = (props) => {
+  // for(var i = 0; i < props.data.length; i++){
+  //   props.data.buckets[i].gender.buckets;
+  // }
+
+  const data = {
+    labels: ['0-100', '101-1000', '1001-10,000', '10,001-100,000', '100,001-1,000,000', '1,000,000+'],
+    datasets: [
+      {
+        label: 'Women',
+        backgroundColor: 'rgba(104, 142, 234,0.2)',
+        borderColor: 'rgba(104, 142, 234,1)',
+        borderWidth: 1,
+        hoverBackgroundColor: 'rgba(104, 142, 234,0.4)',
+        hoverBorderColor: 'rgba(104, 142, 234,1)',
+        data: [props.data.buckets[0].gender.buckets[2].doc_count, 100, 100, 100, 100, 100, 100],
+      },
+      {
+        label: 'Men',
+        backgroundColor: 'rgba(255,99,132,0.2)',
+        borderColor: 'rgba(255,99,132,1)',
+        borderWidth: 1,
+        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+        hoverBorderColor: 'rgba(255,99,132,1)',
+        data: [65, 59, 80, 81, 56, 55, 40],
+      }
+    ],
+  };
+  console.log(props.data);
+  return <Bar data={data} width="40" height="60" />;
+  //return <div>JSON.stringify(props.data)</div>
+};
+
+export default TCEHistogram;
--- src/chartComponents/twitterDoughnut.jsx
+++ src/chartComponents/twitterDoughnut.jsx
@@ -3,8 +3,13 @@ import { Doughnut } from 'react-chartjs-2';
 
 const TwitterDoughnut = (props) => {
   // console.log(props.data);
-  const womenPercent = (props.data[1].doc_count / props.data[0].doc_count) * 100;
-  const menPercent = (props.data[3].doc_count / props.data[4].doc_count) * 100;
+  const totalFemaleSenderTweets = props.data[0];
+  const totalFemaleSenderTweetsWithKeyword = props.data[1];
+  const totalMaleSenderTweetsWithKeyword = props.data[3];
+  const totalMaleSenderTweets = props.data[4];
+
+  const womenPercent = (totalFemaleSenderTweetsWithKeyword.doc_count / totalFemaleSenderTweets.doc_count) * 100;
+  const menPercent = (totalMaleSenderTweetsWithKeyword.doc_count / totalMaleSenderTweets.doc_count) * 100;
   return <Doughnut data={{ datasets: [{ data: [womenPercent, menPercent], backgroundColor: ['blue', 'hotpink'] }], labels: ['Women', 'Men'] }} width="40" height="60" />;
 };
 
