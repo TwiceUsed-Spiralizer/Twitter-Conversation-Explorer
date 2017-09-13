f5343306b6505d09491ec36e165d0009b94aa080
P-WM <P-WM@users.noreply.github.com>
2017-09-13 16:19:43 +0100

    Move items to board

--- src/chartComponents/ChartComponent.jsx
+++ src/chartComponents/ChartComponent.jsx
@@ -6,46 +6,36 @@ import ChiSquared from './chiSquared';
 import LineGraph from './lineGraph';
 import Histogram from './histogram';
 
-export default (Wrapper, onClick) => {
+export default (Wrapper, clickHandler = () => {}) => {
+
   return (chartObject, index) => {
     if (!chartObject.data) {
       return <Wrapper><Container><Preloader size="big" flashing /></Container></Wrapper>;
     }
+    let Chart;
     switch (chartObject.type) {
       case 'doughnut':
-        return (<Wrapper
-          icon={chartObject.icon}
-          title={<Icon left>{chartObject.icon}</Icon>}
-          onClick={() => onClick(index)}
-        >
-          <TwitterDoughnut data={chartObject.data} />
-        </Wrapper>);
+        Chart = TwitterDoughnut;
+        break;
       case 'line':
-        return (<Wrapper
-          icon={chartObject.icon}
-          title={<Icon left>{chartObject.icon}</Icon>}
-          onClick={() => onClick(index)}
-        >
-          <LineGraph data={chartObject.data} />
-        </Wrapper>);
+        Chart = LineGraph;
+        break;
       case 'histogram':
-        return (<Wrapper
-          icon={chartObject.icon}
-          title={<Icon left>{chartObject.icon}</Icon>}
-          onClick={() => onClick(index)}
-        >
-          <Histogram data={chartObject.data} />
-        </Wrapper>);
+        Chart = Histogram;
+        break;
       case 'chiSquared':
-        return (<Wrapper
-          icon={chartObject.icon}
-          title={<Icon left>{chartObject.icon}</Icon>}
-          onClick={() => onClick(index)}
-        >
-          <ChiSquared data={chartObject.data} />
-        </Wrapper>);
+        Chart = ChiSquared;
+        break;
       default:
-        return null;
+        Chart = LineGraph;
+        break;
     }
+    return (<Wrapper
+      icon={chartObject.icon}
+      title={<Icon left>{chartObject.icon}</Icon>}
+      onClick={() => clickHandler(index)}
+    >
+      <Chart data={chartObject.data} />
+    </Wrapper>);
   }
 }
