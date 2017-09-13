4639e4cfd82a20e9b4e0064193189cd194876fd7
gbatra17 <gbatra17@gmail.com>
2017-09-13 14:03:23 -0400

    gutted server codebase

--- src/chartComponents/twitterDoughnut.jsx
+++ src/chartComponents/twitterDoughnut.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
 const TwitterDoughnut = (props) => {
-  <Doughnut data={{ datasets: [{ data: [props.data.womenLength, props.data.menLength], backgroundColor: ['blue', 'hotpink'] }], labels: ['Women', 'Men'] }} />;
+  return null;
+  // <Doughnut data={{ datasets: [{ data: [props.data.womenLength, props.data.menLength], backgroundColor: ['blue', 'hotpink'] }], labels: ['Women', 'Men'] }} />;
 };
 
 export default TwitterDoughnut;
