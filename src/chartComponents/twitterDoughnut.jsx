ea9a17de34604b4b1fbc78ab3316c9527cfb4249
gbatra17 <gbatra17@gmail.com>
2017-09-13 14:22:05 -0400

    added Doughtnut computation

--- src/chartComponents/twitterDoughnut.jsx
+++ src/chartComponents/twitterDoughnut.jsx
@@ -1,5 +1,11 @@
 const TwitterDoughnut = (props) => {
+  const data = props.data;
   console.log(props.data);
+  const womenPercent = (data.femaleSorry / data.female) * 100;
+  const menPercent = (data.maleSorry / data.male) * 100;
+  const total = womenPercent + menPercent;
+  const womenLength = (womenPercent / total) * 600;
+  const menLength = (menPercent / total) * 600;
   return null;
   // <Doughnut data={{ datasets: [{ data: [props.data.womenLength, props.data.menLength], backgroundColor: ['blue', 'hotpink'] }], labels: ['Women', 'Men'] }} />;
 };
