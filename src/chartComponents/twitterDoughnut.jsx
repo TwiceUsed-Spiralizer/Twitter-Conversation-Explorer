de4a164b674f812f52410be34359ac282d1d4b1d
gbatra17 <gbatra17@gmail.com>
2017-09-13 17:35:38 -0400

    Rendering TwitterDoughnut

--- src/chartComponents/twitterDoughnut.jsx
+++ src/chartComponents/twitterDoughnut.jsx
@@ -2,14 +2,10 @@ import React from 'react';
 import { Doughnut } from 'react-chartjs-2';
 
 const TwitterDoughnut = (props) => {
-  console.log(props.data);
-
+  // console.log(props.data);
   const womenPercent = (props.data[1].doc_count / props.data[0].doc_count) * 100;
   const menPercent = (props.data[3].doc_count / props.data[4].doc_count) * 100;
-  const total = womenPercent + menPercent;
-  const womenLength = (womenPercent / total) * 600;
-  const menLength = (menPercent / total) * 600;
-  return <Doughnut data={{ datasets: [{ data: [womenLength, menLength], backgroundColor: ['blue', 'hotpink'] }], labels: ['Women', 'Men'] }} />;
+  return <Doughnut data={{ datasets: [{ data: [womenPercent, menPercent], backgroundColor: ['blue', 'hotpink'] }], labels: ['Women', 'Men'] }} width="40" height="60" />;
 };
 
 export default TwitterDoughnut;
