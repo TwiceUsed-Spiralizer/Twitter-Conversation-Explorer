dd44755066bf2b09c66fce3f8c80af57a7ec9875
P-WM <P-WM@users.noreply.github.com>
2017-09-13 04:48:50 +0100

    Use higher order components to make saveable charts

--- src/chartComponents/index.js
+++ src/chartComponents/index.js
@@ -1,11 +1,23 @@
+import React from 'react';
+
 import TwitterDoughnut from './twitterDoughnut';
 import ChiSquared from './chiSquared';
 import LineGraph from './lineGraph';
 import Histogram from './histogram';
 
-export {
-  TwitterDoughnut,
-  ChiSquared,
-  LineGraph,
-  Histogram,
-};
+export default (Wrapper) => {
+  return (chartObject) => {
+    switch (chartObject.type) {
+      case 'doughnut':
+        return <Wrapper><TwitterDoughnut data={chartObject.data} /></Wrapper>;
+      case 'line':
+        return <Wrapper><LineGraph data={chartObject.data} /></Wrapper>;
+      case 'histogram':
+        return <Wrapper><Histogram data={chartObject.data} /></Wrapper>;
+      case 'chiSquared':
+        return <Wrapper><ChiSquared data={chartObject.data} /></Wrapper>;
+      default:
+        return null;
+    }
+  }
+}
