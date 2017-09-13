91af917fb593c3341b63e68602a9b80b5fcd0ae4
P-WM <P-WM@users.noreply.github.com>
2017-09-13 02:08:44 +0100

    Add example use of TwitterDoughnut in QueryResults

--- src/QueryResults.jsx
+++ src/QueryResults.jsx
@@ -10,7 +10,6 @@ export default class QueryResults extends React.Component {
   }
 
   componentWillReceiveProps(nextProps) {
-    console.log('change', nextProps);
     this.setState({
       carousel: null,
     })
