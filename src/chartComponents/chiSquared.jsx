b2bcd2736c7b1cabcf7b7b973dcd118bca93db53
gbatra17 <gbatra17@gmail.com>
2017-09-14 03:07:42 -0400

    Rendering Line Graph

--- src/chartComponents/chiSquared.jsx
+++ src/chartComponents/chiSquared.jsx
@@ -1,8 +1,38 @@
 import React from 'react';
+import 'react-table/react-table.css';
+import ReactTable from 'react-table';
 
-export default props =>
-  (
-    <div className="hoverable">
-      {JSON.stringify(props.data)}
-    </div>
-  );
+const ChiSquared = (props) => {
+  const data = [{
+    name: 'Tanner Linsley',
+    age: 26,
+    chi_squared: 'keyword',
+    chi_squared: 'not keyword',
+  }];
+
+  const columns = [{
+    Header: 'Chi Squared',
+    id: 'chi_squared',
+    accessor: a => a.chi_squared,
+  },
+  {
+    Header: 'Male',
+    accessor: 'male',
+  }, {
+    Header: 'Female',
+    accessor: 'female',
+  }];
+
+  console.log(props.data);
+  return (<ReactTable
+    data={data}
+    noDataText="Oh Noes!"
+    showPagination={false}
+    resizable={false}
+    defaultPageSize={2}
+    columns={columns}
+    className="-striped -highlight"
+  />);
+};
+
+export default ChiSquared;
--- src/chartComponents/lineGraph.jsx
+++ src/chartComponents/lineGraph.jsx
@@ -1,8 +1,34 @@
 import React from 'react';
+import { Line } from 'react-chartjs-2';
+import moment from 'moment';
 
-export default props =>
-  (
-    <div className="hoverable">
-      {JSON.stringify(props.data)}
-    </div>
-  );
+const TCELineGraph = (props) => {
+  const allDatesFormatted = props.data.map(obj => moment(obj.key).fromNow());
+  const allDataFormatted = props.data.map(obj => obj.doc_count);
+  const data = {
+    labels: allDatesFormatted,
+    datasets: [
+      {
+        label: 'Tweets For Keyword By Time',
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: 'rgba(75,192,192,0.4)',
+        borderColor: 'rgba(75,192,192,1)',
+        pointBorderColor: 'rgba(75,192,192,1)',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: allDataFormatted,
+      },
+    ],
+  };
+  console.log(props);
+  return <Line data={data} />;
+};
+
+export default TCELineGraph;
