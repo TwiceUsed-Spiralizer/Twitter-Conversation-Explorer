1a0bc0c6894bd43bfde1575e7ada84a591c2bff1
P-WM <P-WM@users.noreply.github.com>
2017-09-14 19:22:08 +0100

    WIP: Fix presentation of carousel

--- src/QueryResults.jsx
+++ src/QueryResults.jsx
@@ -1,19 +1,39 @@
 import React from 'react';
 import Slider from 'react-slick';
+import { Button, Container } from 'react-materialize';
 import './QueryResults.css';
 import ChartComponent from './chartComponents';
 import CarouselChart from './ChartWrappers/CarouselChart';
 
+const PrevButton = (props) => {
+  return (
+    <Button floating large icon="arrow_back" id="carousel-prev" className="red" style={props.style} onClick={props.onClick} />
+  );
+};
+
+const NextButton = (props) => {
+  return (
+    <Button floating large icon="arrow_forward" id="carousel-next" className="red" style={props.style} onClick={props.onClick} />
+  );
+};
+
 export default (props) => {
   console.log(props);
+  if (!props.results.length) return <div id="results-placeholder" className="teal lighten-4"><h1>Welcome to TweetInsight</h1></div>;
   const chartWrapper = CarouselChart(props.moveToBoard);
   return (
     <div id="results-carousel">
-      <Slider adaptiveHeight={false} autoplay={true} dots={true} arrows={false} >
-        {
-          props.results.map(ChartComponent(chartWrapper))
-        }
+      <Slider adaptiveHeight={false} dots={true} prevArrow={<PrevButton />} nextArrow={<NextButton />} >
+
+ 
+          {
+            props.results.map(ChartComponent(chartWrapper))
+          }
+    
+ 
       </Slider>
     </div>
   );
 };
+
+//       <div style={{ height: '450px', width: '80%' }}>
