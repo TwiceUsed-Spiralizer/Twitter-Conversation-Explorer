17816747bb6e42da57abbd957c880d9445f8a6d3
P-WM <P-WM@users.noreply.github.com>
2017-09-14 20:47:25 +0100

    Prevent rendering of empty histogram

--- src/chartComponents/histogram.jsx
+++ src/chartComponents/histogram.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import { Bar } from 'react-chartjs-2';
 
 const TCEHistogram = (props) => {
-  //console.log(props.data);
   const arrayOfDataForFemales = props.data.buckets.map(el => (el.gender.buckets[2] ? el.gender.buckets[2].doc_count : 0));
   const arrayOfDataForMales = props.data.buckets.map(el => (el.gender.buckets[1] ? el.gender.buckets[1].doc_count : 0));
+  const allZeroes = (array) => {
+    return array.every(item => item === 0);
+  }
+  if (allZeroes(arrayOfDataForMales.concat(arrayOfDataForFemales))) {
+    return (<div>Sorry, there are no tweets with the keyword {props.keyword} in our database!</div>);
+  }
 
   const dataForGraph = {
     labels: ['0-100', '101-1000', '1001-10,000', '10,001-100,000', '100,001-1,000,000', '1,000,000+'],
