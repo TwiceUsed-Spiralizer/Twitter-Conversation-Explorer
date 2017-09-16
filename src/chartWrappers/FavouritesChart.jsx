22d14bc38b86432c486f8eb7d0e93a2ba9ab7e80
P-WM <P-WM@users.noreply.github.com>
2017-09-17 00:14:07 +0100

    Add style and defavourite handler to Favourites

--- src/chartWrappers/FavouritesChart.jsx
+++ src/chartWrappers/FavouritesChart.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import { Button, Col, Row, Icon, Card } from 'react-materialize';
+import { Button, Col, Row, Icon, Card, Chip } from 'react-materialize';
 import './FavouritesChart.css';
 
-export default clickHandler =>
+export default unfavourite =>
   props => (
     <Col m={4}>
       <Card horizontal header={<div style={{ height: '300px', margin: '10px' }}>{props.children}</div>} title={<div><Icon left>{props.icon}</Icon> {props.chartObject.keyword}</div>}>
         {props.title}
-        <Row>
-          <Col m={3}>
-            <Icon className="starred">star</Icon>
+        <Row id="favourite-button-group">
+          <Col m={4}>
+            <Chip><div onClick={() => unfavourite(props.chartObject.id)}><Icon small className="favourite-button starred" /></div></Chip>
           </Col>
-          <Col m={3}>
-            <Icon>play_for_work</Icon>
+          <Col m={4}>
+            <Chip><Icon small className="favourite-button">play_for_work</Icon></Chip>
           </Col>
-          <Col m={3}>
-            <Icon>share</Icon>
+          <Col m={4}>
+            <Chip><Icon small className="favourite-button">share</Icon></Chip>
           </Col>
         </Row>
       </Card>
--- src/containers/Favourites.jsx
+++ src/containers/Favourites.jsx
@@ -4,13 +4,23 @@ import { Card } from 'react-materialize';
 import ChartComponent from '../chartComponents';
 
 const Favourites = props => (
+<<<<<<< 4a4eaa53e01e926242a2ea956172e5380656172f
   <div>
     {props.favourites.map(ChartComponent(Card))}
   </div>
+=======
+  <Row>
+    {props.favourites.map(ChartComponent(FavouritesChart(props.unfavourite)))}
+  </Row>
+>>>>>>> Add style and defavourite handler to Favourites
 );
 
 const mapStateToProps = state => ({
   favourites: Array.from(state.favourites).map(key => state.charts[key]),
 });
 
-export default connect(mapStateToProps)(Favourites);
+const mapDispatchToProps = dispatch => ({
+  unfavourite: id => dispatch({ type: 'FAVOURITES_DELETE', id }),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Favourites);
