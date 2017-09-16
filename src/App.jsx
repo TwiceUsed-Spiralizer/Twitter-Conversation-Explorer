6954717ec6482cedca9db3c7a745d92d42332425
P-WM <P-WM@users.noreply.github.com>
2017-09-16 04:36:13 +0100

    Make app stateless

--- src/App.jsx
+++ src/App.jsx
@@ -4,37 +4,31 @@ import { Route, Switch, Link } from 'react-router-dom';
 import { Query, Favourites, Board } from './containers';
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    super();
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <nav>
-          <Navbar brand={<Link to="/">Twitter Conversation Explorer</Link>} right>
-            <Button waves="light">Log In<Icon right>face</Icon></Button>
-            <Button waves="light" >Sign Up<Icon right>face</Icon></Button>
-            <Dropdown trigger={
-              <Button waves="light"><Icon right>menu</Icon><Badge newIcon>4</Badge></Button>
-            }
-            >
-              <NavItem>Profile</NavItem>
-              <NavItem>Dashboard</NavItem>
-              <NavItem>Saved Searches</NavItem>
-              <NavItem>Logout</NavItem>
-            </Dropdown>
-          </Navbar>
-        </nav>
-        <Switch>
-          <Route exact path="/" component={Query} />
-          <Route exact path="/favourites" component={Favourites} />
-          <Route exact path="/board/:boardName" component={Board} />
-        </Switch>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="App">
+      <nav>
+        <Navbar brand={<Link to="/">Twitter Conversation Explorer</Link>} right>
+          <Button waves="light">Log In<Icon right>face</Icon></Button>
+          <Button waves="light" >Sign Up<Icon right>face</Icon></Button>
+          <Dropdown trigger={
+            <Button waves="light"><Icon right>menu</Icon><Badge newIcon>4</Badge></Button>
+          }
+          >
+            <NavItem>Profile</NavItem>
+            <NavItem>Dashboard</NavItem>
+            <NavItem>Saved Searches</NavItem>
+            <NavItem>Logout</NavItem>
+          </Dropdown>
+        </Navbar>
+      </nav>
+      <Switch>
+        <Route exact path="/" component={Query} />
+        <Route exact path="/favourites" component={Favourites} />
+        <Route exact path="/board/:boardName" component={Board} />
+      </Switch>
+    </div>
+  );
+};
 
 export default App;
