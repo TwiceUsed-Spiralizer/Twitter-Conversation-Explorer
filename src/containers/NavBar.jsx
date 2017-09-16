5c36f702380c06b8f696a803311c950e393c3c10
gbatra17 <gbatra17@gmail.com>
2017-09-16 18:22:30 -0400

    Added create new board functionality

--- src/containers/NavBar.jsx
+++ src/containers/NavBar.jsx
@@ -9,30 +9,37 @@ class NavBar extends Component {
     this.state = {
       boardName: '',
     };
+    this.createBoard = this.createBoard.bind(this);
+  }
+
+  createBoard() {
+    this.props.createBoard(this.state.boardName);
   }
 
   render() {
     return (
       <Navbar brand={<Link to="/">Tweet Insight</Link>} right>
-        <Button waves="light">Log In</Button>
-        <Button waves="light" >Sign Up</Button>
-        <Modal
-          header="Add New Board"
-          trigger={<Button icon="add_box" />}
-        >
-          <Input label="Enter a Boardname" s={12} onChange={event => this.setState({ boardName: event.target.value })} />
-        </Modal>
         <Row>
-          <Input s={3} type="select" label="BoardName" defaultValue={1} onChange={event => this.setState({ boardName: event.target.value })} />
+          <Modal
+            header="Add New Board"
+            trigger={<NavItem><Icon>add_box</Icon></NavItem>}
+          >
+            <Input label="Enter a Boardname" s={12} onChange={event => this.setState({ boardName: event.target.value })} />
+            <Button onClick={this.createBoard}>Create New Board</Button>
+          </Modal>
+          <Input s={3} type="select" label="BoardName">
+          {this.props.boards.map(item => <option>{item}</option>)}
+          </Input>
+          <NavItem>Log In</NavItem>
+          <NavItem>Sign Up</NavItem>
         </Row>
-        {/* <Button waves="light"><Link to="/board">Add New Board</Link></Button> */}
       </Navbar>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  favourites: Array.from(state.favourites).map(key => state.charts[key]),
+  boards: Object.keys(state.boards),
 });
 
 const mapDispatchToProps = dispatch => ({
