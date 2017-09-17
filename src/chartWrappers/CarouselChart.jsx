13cef5c98b21202607fe2f3665c1c43f2759cee7
P-WM <P-WM@users.noreply.github.com>
2017-09-17 04:43:01 +0100

    Tweak colours and style

--- src/chartWrappers/CarouselChart.jsx
+++ src/chartWrappers/CarouselChart.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Button, Icon } from 'react-materialize';
+import { Button, Icon, Card } from 'react-materialize';
 
 export default clickHandler =>
   props => (
-    <div {...props} title={<Icon left>{props.icon}</Icon>}>
+    <Card {...props} title={<Icon left>{props.icon}</Icon>}>
       <h5 style={{ textAlign: 'center' }}>{props.title}</h5>
         <div style={{ height: '750px', width: '75%', margin: 'auto' }}>{props.children}</div>
       {props.chartObject && <Button onClick={() => clickHandler(props.chartObject)}><Icon left>save</Icon>{props.chartObject.favourited ? 'Saved' : 'Save'}</Button>}
-    </div>
+    </Card>
   );
