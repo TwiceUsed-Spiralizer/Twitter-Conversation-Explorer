d26d8f7c13a4eae6a91e98c6c2873793e76fa7ea
P-WM <P-WM@users.noreply.github.com>
2017-09-17 02:36:40 +0100

    Add modal to favourites

--- src/chartWrappers/FavouritesChart.jsx
+++ src/chartWrappers/FavouritesChart.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Col, Row, Icon, Card, Chip } from 'react-materialize';
+import { Col, Row, Icon, Card, Chip, Modal, Button } from 'react-materialize';
+import BareChartComponent from '../chartComponents/BareChartComponent';
 import './FavouritesChart.css';
 
 export default (unfavourite, pinToBoard) =>
@@ -8,14 +9,21 @@ export default (unfavourite, pinToBoard) =>
       <Card horizontal header={<div style={{ height: '300px', margin: '10px' }}>{props.children}</div>} title={<div><Icon left>{props.icon}</Icon> {props.chartObject.keyword}</div>}>
         {props.title}
         <Row id="favourite-button-group">
-          <Col m={4}>
+          <Col m={3}>
             <Chip><div onClick={() => unfavourite(props.chartObject.id)}><Icon small className="favourite-button starred" /></div></Chip>
           </Col>
-          <Col m={4}>
+          <Col m={3}>
             <Chip><Icon small className="favourite-button">play_for_work</Icon></Chip>
           </Col>
-          <Col m={4}>
+          <Col m={3}>
             <Chip><Icon small className="favourite-button">share</Icon></Chip>
+          </Col>
+          <Col m={3}>
+            <Chip>
+              <Modal trigger={<div><Icon small className="favourite-button">zoom_in</Icon></div>}id="chart-modal" header={props.chartObject.title}>
+                <div style={{ height: '500px' }}>{BareChartComponent(props.chartObject)}</div>
+              </Modal>
+            </Chip>
           </Col>
         </Row>
       </Card>
