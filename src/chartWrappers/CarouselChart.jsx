a561dfb2966bb90ff0a6f1aa8695c9b317ddf511
P-WM <P-WM@users.noreply.github.com>
2017-09-18 00:40:46 +0100

    Implement basic features on QueryResults component

--- src/chartWrappers/CarouselChart.jsx
+++ src/chartWrappers/CarouselChart.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
-import { Button, Icon, Card } from 'react-materialize';
+import { Button, Icon, Card, Row, Col } from 'react-materialize';
+import { BoardPinModal } from '../components';
 
-export default clickHandler =>
+export default favourite =>
   props => (
     <Card {...props} title={<Icon left>{props.icon}</Icon>}>
       <h5 style={{ textAlign: 'center' }}>{props.title}</h5>
       <div style={{ height: '750px', width: '75%', margin: 'auto' }}>{props.children}</div>
-      {props.chartObject &&
-        props.chartObject.favourited
-        ? <Button><Icon left style={{ color: 'gold' }}>star</Icon>Favourited</Button>
-        : <Button onClick={() => clickHandler(props.chartObject)}><Icon left>star_border</Icon> Favourite</Button>
-      }
+      <Row>
+        <Col>
+          {
+            props.chartObject.favourited
+              ? <Button><Icon left style={{ color: 'gold' }}>star</Icon>Favourited</Button>
+              : <Button onClick={() => favourite(props.chartObject)}><Icon left>star_border</Icon> Favourite</Button>
+          }
+        </Col>
+        <Col>
+          <Button waves="purple"><Icon left>share</Icon> Share</Button>
+        </Col>
+        <Col>
+          <BoardPinModal
+            trigger={<Button waves="purple"><Icon left>play_for_work</Icon> Pin</Button>}
+            chartObject={props.chartObject}
+          />
+        </Col>
+      </Row>
     </Card>
   );
