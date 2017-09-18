04f590aceae1ac3471bbd41724392f414a80881a
gbatra17 <gbatra17@gmail.com>
2017-09-18 18:20:16 -0400

    Removed canvas component

--- src/components/QueryResults.jsx
+++ src/components/QueryResults.jsx
@@ -8,13 +8,13 @@ import CarouselChart from '../chartWrappers/CarouselChart';
 
 const PrevButton = (props) => {
   return (
-    <Button floating large icon="arrow_back" id="carousel-prev" className="red" style={props.style} onClick={props.onClick} />
+    <Button floating large icon="arrow_back" id="carousel-prev" className="red" onClick={props.onClick} />
   );
 };
 
 const NextButton = (props) => {
   return (
-    <Button floating large icon="arrow_forward" id="carousel-next" className="red" style={props.style} onClick={props.onClick} />
+    <Button floating large icon="arrow_forward" id="carousel-next" className="red" onClick={props.onClick} />
   );
 };
 
