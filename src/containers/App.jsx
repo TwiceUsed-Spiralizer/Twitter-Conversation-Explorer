725f3e8901569103ef5a2e7b58976f0a5e383280
P-WM <P-WM@users.noreply.github.com>
2017-09-19 01:58:36 +0100

    Add barebones firebase auth to frontend

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { Query, Favourites, Board, NavBar } from './index';
+import { connect } from 'react-redux';
+import { Query, Favourites, Board, NavBar, Auth } from './index';
 import './App.css';
+import firebase from '../firebase';
 
-const App = () => {
-  return (
-    <div className="App">
-      <NavBar />
-      <Switch>
-        <Route exact path="/" component={Query} />
-        <Route exact path="/favourites" component={Favourites} />
-        <Route path="/board/:boardName" component={Board} />
-      </Switch>
-    </div>
-  );
-};
+class App extends React.Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(this.props.login);
+  }
 
-export default App;
+  render() {
+    return (
+      <div className="App">
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={Query} />
+          <Route exact path="/favourites" component={Favourites} />
+          <Route path="/board/:boardName" component={Board} />
+          <Route path="/auth" component={Auth} />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  login: user => dispatch({ type: 'LOGIN', user }),
+  logout: () => dispatch({ type: 'LOGOUT' }),
+});
+
+export default connect(null, mapDispatchToProps)(App);
