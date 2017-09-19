776fe51f815a585bde626fb9714e6d1c239b8009
P-WM <P-WM@users.noreply.github.com>
2017-09-20 00:10:15 +0100

    Make GuestNav stateless

--- src/components/GuestNav.jsx
+++ src/components/GuestNav.jsx
@@ -1,40 +1,12 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Navbar, NavItem, Dropdown, Button, Icon, Modal, Input, Row } from 'react-materialize';
+import React from 'react';
+import { Navbar, NavItem, Icon, Row } from 'react-materialize';
 import { Link } from 'react-router-dom';
-import firebase from '../firebase';
 
-class GuestNav extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      boardName: '',
-    };
-  }
-
-  componentDidMount() {
-    console.log('mount')
-    console.log(document.getElementById('auth-modal'));
-  }
-
-  render() {
-    return (
-      <Navbar brand={<Link to="/">Tweet Insight</Link>} right style={{ 'margin-bottom': '20px', 'padding-left': '20px' }}>
-        <Row>
-          <NavItem><Link to="/login"><Icon right>account_circle</Icon>Login</Link></NavItem>
-        </Row>
-      </Navbar>
-    );
-  }
-}
-
-const mapStateToProps = state => ({
-  boards: state.boards ? Object.keys(state.boards) : [],
-  user: state.user,
-});
-
-const mapDispatchToProps = dispatch => ({
-  createBoard: boardName => dispatch({ type: 'BOARD_CREATE', boardName }),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(GuestNav);
+export default () =>
+  (
+    <Navbar brand={<Link to="/">Tweet Insight</Link>} right style={{ 'margin-bottom': '20px', 'padding-left': '20px' }}>
+      <Row>
+        <NavItem><Link to="/login"><Icon right>account_circle</Icon>Login</Link></NavItem>
+      </Row>
+    </Navbar>
+  );
