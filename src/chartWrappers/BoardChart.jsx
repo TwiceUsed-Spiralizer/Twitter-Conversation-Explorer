621afbca2956bbef1a7d85ab10b91573adfcb0ae
gbatra17 <gbatra17@gmail.com>
2017-09-18 22:58:25 -0400

    Got icons placed correctly in chart

--- src/chartWrappers/BoardChart.jsx
+++ src/chartWrappers/BoardChart.jsx
@@ -1,32 +1,37 @@
 import React from 'react';
 import { Col, Row, Icon, Card, Chip, Modal } from 'react-materialize';
 import BareChartComponent from '../chartComponents/BareChartComponent';
-import './FavouritesChart.css';
+import './BoardChart.css';
 
 export default (unfavourite, BoardPinDropdown) =>
   props => (
-    <Col m={12} s={12}>
-      <Card horizontal header={<div style={{ height: '300px', margin: '10px' }}>{props.children}</div>} title={<div><Icon left>{props.icon}</Icon> {props.chartObject.keyword}</div>}>
+    <Col m={12} s={12}>   
+      <Card horizontal header={<div style={{ height: '300px' }}>{props.children}</div>} title={<div><Icon left>{props.icon}</Icon> {props.chartObject.keyword}</div>}>
         {props.title}
         <Row id="favourite-button-group">
-          <Col m={3}>
+          
+          <Col s={4}>
             <Chip><div onClick={() => unfavourite(props.chartObject.id)}><Icon small className="favourite-button starred" /></div></Chip>
           </Col>
-          <Col m={3}>
+          
+          <Col s={4}>
             <Chip>
               <BoardPinDropdown trigger={<div><Icon small className="favourite-button">play_for_work</Icon></div>} chartObject={props.chartObject} />
             </Chip>
           </Col>
-          <Col m={3}>
+          
+          <Col s={4}>
             <Chip><Icon small className="favourite-button">share</Icon></Chip>
           </Col>
-          <Col m={3}>
+
+          <Col s={4}>
             <Chip>
               <Modal trigger={<div><Icon small className="favourite-button">zoom_in</Icon></div>}id="chart-modal" header={props.chartObject.title}>
                 <div style={{ height: '500px' }}>{BareChartComponent(props.chartObject)}</div>
               </Modal>
             </Chip>
           </Col>
+
         </Row>
       </Card>
     </Col>
