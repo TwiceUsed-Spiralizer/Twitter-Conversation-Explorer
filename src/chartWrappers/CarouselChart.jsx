a015a68da77d4ad800c15f2607b876933859dae1
P-WM <P-WM@users.noreply.github.com>
2017-09-20 19:48:43 +0100

    Show embed info on carousel

--- src/chartWrappers/CarouselChart.jsx
+++ src/chartWrappers/CarouselChart.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Icon, Card, Row, Col } from 'react-materialize';
+import { Button, Icon, Card, Row, Col, Input } from 'react-materialize';
 import { BoardPinModal } from '../components';
 
 export default (favourite, embed, authenticated) =>
@@ -26,6 +26,14 @@ export default (favourite, embed, authenticated) =>
               chartObject={props.chartObject}
             />
           </Col>
+          {props.chartObject.embedId && <Col>
+            <Input
+              label="Embed code"
+              defaultValue={`<iframe width="560" height="315" src="http://twinsighttest-env.eiwejfj7bv.us-east-2.elasticbeanstalk.com/embed/${props.chartObject.embedId}" frameborder="0" ></iframe>`}
+            >
+              <Icon>share</Icon>
+            </Input>
+          </Col>}
         </Row>)
       }
     </Card>
