b04fc5cc0208beb3f18824f3af133828d7bb5d1f
P-WM <P-WM@users.noreply.github.com>
2017-09-20 20:07:29 +0100

    Tweak embed for reusability

--- src/firebase/embed.js
+++ src/firebase/embed.js
@@ -1,9 +1,19 @@
 import firebase from './index';
 
+let chartsRef;
 const embedRef = firebase.database().ref('/embeds');
+firebase.auth().onAuthStateChanged((user) => {
+  chartsRef = firebase.database().ref(`/charts/${user.uid}/`)
+});
+
 const embed = (chartObject) => {
   const { resultsIndex, ...restOfObject } = chartObject;
-  return embedRef.push(restOfObject).then(item => item.key);
+  return embedRef.push(restOfObject).then((item) => {
+    if (chartObject.id) {
+      return chartsRef.child(`${chartObject.id}/embedId`).set(item.key).then(() => item.key);
+    }
+    return item.key;
+  });
 };
 
 export default embed;
