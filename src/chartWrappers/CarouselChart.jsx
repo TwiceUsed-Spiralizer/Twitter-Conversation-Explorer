ef292bf402ad2187f8a1eddb20cc236d21305085
P-WM <P-WM@users.noreply.github.com>
2017-09-21 16:10:46 +0100

    Add EmbedModal to QueryResults

--- src/chartWrappers/CarouselChart.jsx
+++ src/chartWrappers/CarouselChart.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button, Icon, Card, Row, Col, Input } from 'react-materialize';
-import { BoardPinModal } from '../components';
+import { BoardPinModal, EmbedModal } from '../components';
 
 export default (favourite, embed, authenticated) =>
   props => (
@@ -17,7 +17,7 @@ export default (favourite, embed, authenticated) =>
             }
           </Col>
           <Col>
-            <Button waves="purple" onClick={() => embed(props.chartObject)}><Icon left>share</Icon> Share</Button>
+            <EmbedModal trigger={<Button waves="purple"><Icon left>share</Icon> Share</Button>} chartObject={props.chartObject} />
           </Col>
           <Col>
             <BoardPinModal
@@ -26,14 +26,6 @@ export default (favourite, embed, authenticated) =>
               chartObject={props.chartObject}
             />
           </Col>
-          {props.chartObject.embedId && <Col>
-            <Input
-              label="Embed code"
-              defaultValue={`<iframe width="560" height="315" src="http://twinsighttest-env.eiwejfj7bv.us-east-2.elasticbeanstalk.com/embed/${props.chartObject.embedId}" frameborder="0" ></iframe>`}
-            >
-              <Icon>share</Icon>
-            </Input>
-          </Col>}
         </Row>)
       }
     </Card>
