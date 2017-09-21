58a9caaf8d66156a6b34826adae3cf54579665b3
P-WM <P-WM@users.noreply.github.com>
2017-09-21 18:47:46 +0100

    Add BoardTabs container

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,11 +1,13 @@
 import Query from './Query';
 import Board from './Board';
+import BoardTabs from './BoardTabs';
 import Favourites from './Favourites';
 import NavBar from './NavBar';
 
 export {
   Query,
   Board,
+  BoardTabs,
   Favourites,
   NavBar,
 };
