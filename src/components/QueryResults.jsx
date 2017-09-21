2a9506c2e50e2c2448fea7c6522bf10a9e828869
P-WM <P-WM@users.noreply.github.com>
2017-09-21 02:23:02 +0100

    Limit animations on ResultsQuery to first batch

--- src/components/QueryResults.jsx
+++ src/components/QueryResults.jsx
@@ -20,20 +20,41 @@ const NextButton = (props) => {
   );
 };
 
-const QueryResults = (props) => {
-  return props.results.length
-    ? (
-      <div id="results-carousel">
-        <Slider adaptiveHeight={false} dots prevArrow={<PrevButton />} nextArrow={<NextButton />} >
-          {
-            props.results.map(ChartComponent(CarouselChart(props.favouriteItem, props.embedItem, props.user), true))
-          }
-        </Slider>
-      </div>
-    )
-    : (
-      <CardPanel><h2 className="center-align">Welcome to TweetInsight</h2></CardPanel>
-    );
+class QueryResults extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      animation: true,
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.results.length) {
+      this.setState({
+        animation: false,
+      });
+    } else {
+      this.setState({
+        animation: true,
+      });
+    }
+  }
+
+  render() {
+    return this.props.results.length
+      ? (
+        <div id="results-carousel">
+          <Slider adaptiveHeight={false} dots prevArrow={<PrevButton />} nextArrow={<NextButton />} >
+            {
+              this.props.results.map(ChartComponent(CarouselChart(this.props.favouriteItem, this.props.embedItem, this.props.user), this.state.animation))
+            }
+          </Slider>
+        </div>
+      )
+      : (
+        <CardPanel><h2 className="center-align">Welcome to TweetInsight</h2></CardPanel>
+      );
+  }
 };
 
 const mapStateToProps = state => ({
