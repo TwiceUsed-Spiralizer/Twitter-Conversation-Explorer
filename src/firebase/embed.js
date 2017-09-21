ee3f34f74e8c5673b85df4ba2000943033ec4fa2
P-WM <P-WM@users.noreply.github.com>
2017-09-21 04:42:38 +0100

    Add embed toast

--- src/firebase/embed.js
+++ src/firebase/embed.js
@@ -1,3 +1,4 @@
+import Materialize from 'materialize-css';
 import firebase from './index';
 
 let chartsRef;
@@ -6,12 +7,23 @@ firebase.auth().onAuthStateChanged((user) => {
   chartsRef = firebase.database().ref(`/charts/${user.uid}/`)
 });
 
+const toaster = (embedId) => {
+  let toast = document.createElement('span');
+  let closeButton = document.createElement('button');
+  toast.textContent = `<iframe width="560" height="315" src="http://twinsighttest-env.eiwejfj7bv.us-east-2.elasticbeanstalk.com/embed/${embedId}" frameborder="0" ></iframe>     `;
+  closeButton.innerHTML = '\t<button style="color:orange;" class="toast-action" onclick="Materialize.Toast.removeAll()">Undo</button>';
+  toast.appendChild(closeButton);
+  Materialize.toast(toast);
+}
+
 const embed = (chartObject) => {
   const { resultsIndex, ...restOfObject } = chartObject;
   if (chartObject.embedId) {
+    toaster(chartObject.embedId);
     return new Promise(resolve => resolve(chartObject.embedId));
   }
   return embedRef.push(restOfObject).then((item) => {
+    toaster(item.key);
     if (chartObject.id) {
       return chartsRef.child(`${chartObject.id}/embedId`).set(item.key).then(() => item.key);
     }
