303490ccd04cf6bb4789fd131de298c8efa5b85c
P-WM <P-WM@users.noreply.github.com>
2017-09-21 16:12:21 +0100

    Show correct buttons on EmbedModal for results

--- src/components/EmbedModal.jsx
+++ src/components/EmbedModal.jsx
@@ -22,22 +22,23 @@ export default class extends React.Component {
   }
 
   render() {
-    const embedButton = this.props.chartObject.embedId
-      ? <Button className="modal-action btn-flat">Copy code</Button>
-      : <Button className="modal-action btn-flat" onClick={this.getEmbedId}>Generate embed code</Button>;
+    let embedButton;
     let embedContent;
     switch (this.state.embedId) {
       case 'blank':
+        embedButton = <Button className="modal-action btn-flat" onClick={this.getEmbedId}>Generate embed code</Button>;
         embedContent = null;
         break;
       case 'loading':
+        embedButton = null;
         embedContent = <ProgressBar className="pink" />;
         break;
       default:
+        embedButton = <Button className="modal-action btn-flat">Copy code</Button>;
         embedContent = (<Input
           icon="share"
           defaultValue={`<iframe width="560" height="315" src="http://twinsighttest-env.eiwejfj7bv.us-east-2.elasticbeanstalk.com/embed/${this.state.embedId}" frameborder="0" ></iframe>`}
-        />)
+        />);
     }
     return (
       <Modal
