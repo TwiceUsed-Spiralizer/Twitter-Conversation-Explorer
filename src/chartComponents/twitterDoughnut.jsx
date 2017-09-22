12f5c741d5dd7d71c2d502522ceae075ab6ed02b
gbatra17 <gbatra17@gmail.com>
2017-09-21 21:25:46 -0400

    Fixed doughnut

--- src/chartComponents/twitterDoughnut.jsx
+++ src/chartComponents/twitterDoughnut.jsx
@@ -6,11 +6,6 @@ const TwitterDoughnut = (props) => {
     return (<div>Sorry, there are no tweets with the keyword {props.keyword} in our database!</div>);
    }  
 
-<<<<<<< ac20bffc235f992b63390c44d279854351e276bf
-  const womenPercent = (totalFemaleSenderTweetsWithKeyword.doc_count / totalFemaleSenderTweets.doc_count) * 100;
-  const menPercent = (totalMaleSenderTweetsWithKeyword.doc_count / totalMaleSenderTweets.doc_count) * 100;
-  return (<Doughnut options={props.options} data={{ datasets: [{ data: [womenPercent, menPercent], backgroundColor: ['#F9CF00', '#F19F4D'] }], labels: ['Women', 'Men'] }} />);
-=======
   const dataFromProps = props.data;
   const categoryATotal = dataFromProps[`aaa_${props.params.dataNameA}`];
   const categoryAandKeyword = dataFromProps[`aaa_${props.params.dataNameA}&keyword`];
@@ -20,7 +15,6 @@ const TwitterDoughnut = (props) => {
   const categoryAPercent = (categoryAandKeyword / categoryATotal) * 100;
   const categoryBPercent = (categoryBandKeyword / categoryBTotal) * 100;
   return (<Doughnut options={props.options} data={{ datasets: [{ data: [categoryAPercent, categoryBPercent], backgroundColor: ['blue', 'hotpink'] }], labels: [props.params.columnA, props.params.columnB] }} />);
->>>>>>> Working doughtnut charts
 };
 
 export default TwitterDoughnut;
