4b04a0d6853b523688e1f3c25d8ae592f06a5294
P-WM <P-WM@users.noreply.github.com>
2017-09-22 15:57:09 +0100

    Implement rename functionality and close modals

--- src/components/BoardDeleteModal.jsx
+++ src/components/BoardDeleteModal.jsx
@@ -6,7 +6,7 @@ export default props => (
     trigger={props.trigger}
     header={`Are you sure you wish to delete ${props.boardName}?`}
     actions={[
-      <Button className="btn-flat modal-action red" onClick={props.delete} style={{ marginLeft: '5px', marginRight: '5px' }}>Delete</Button>,
+      <Button className="btn-flat modal-action red" modal="close" onClick={props.delete} style={{ marginLeft: '5px', marginRight: '5px' }}>Delete</Button>,
       <Button modal="close" flat>Cancel</Button>,
     ]}
   >
